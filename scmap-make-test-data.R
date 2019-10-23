6761e9df15aa2f3e27a590eb0b29bc4ff5448a6a
Andrey Solovyev <a.solovyev39@gmail.com>
2019-10-23 10:08:40 +0100

    added SCE object pre-processing step to facilitate workflow incrorporation

--- scmap-make-test-data.R
+++ scmap-make-test-data.R
@@ -21,19 +21,11 @@ option_list = list(
 opt <- wsc_parse_args(option_list, mandatory = c('output_object_file'))
 
 # Once arguments are satisfcatory, load scmap package
-
 suppressPackageStartupMessages(require(SingleCellExperiment))
 suppressPackageStartupMessages(require(scmap))
 
 # Create the test object
-
 SingleCellExperiment <- SingleCellExperiment(assays = list(normcounts = as.matrix(yan)), colData = ann)
-#logcounts(SingleCellExperiment) <- log2(normcounts(SingleCellExperiment) + 1)
-# use gene names as feature symbols
-#rowData(SingleCellExperiment)$feature_symbol <- rownames(SingleCellExperiment)
-#isSpike(SingleCellExperiment, "ERCC") <- grepl("^ERCC-", rownames(SingleCellExperiment))
-# remove features with duplicated names
-#SingleCellExperiment <- SingleCellExperiment[!duplicated(rownames(SingleCellExperiment)), ]
 
 # Print introspective information
 cat(capture.output(SingleCellExperiment), sep='\n')
